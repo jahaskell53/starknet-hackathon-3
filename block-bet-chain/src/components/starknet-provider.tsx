4d3f8a89efef57cd616a8301ba2f14ebf3eda043
Michael Fu <m888fu@gmail.com>
2024-02-04 02:18:31 -0500

    connected to wallet

--- block-bet-chain/src/components/starknet-provider.tsx
+++ block-bet-chain/src/components/starknet-provider.tsx
@@ -14,7 +14,7 @@ import {
 export function StarknetProvider({ children }: { children: ReactNode }) {
   const { connectors } = useInjectedConnectors({
     // Show these connectors if the user has no connector installed.
-    recommended: [argent(), braavos()],
+    recommended: [argent()],
     // Hide recommended connectors if the user has any connector installed.
     includeRecommended: "onlyIfNoConnectors",
     // Randomize the order of the connectors.
